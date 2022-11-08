00ccc7122d92ab9bc47da61e109b0f389ce39f4d
Dandan Guo <110000312+DandanGuo1993@users.noreply.github.com>
2022-11-08 10:31:05 +0800

    Create configs.py

---
+++ configs.py
@@ -0,0 +1,5 @@
+save_dir                    = '.'
+data_dir = {}
+# data_dir['CUB']             = './filelists/CUB/'
+# data_dir['miniImagenet']    = './filelists/miniImagenet/'
+data_dir['cifar']    = './filelists/CIFAR/cifar_fs/'
